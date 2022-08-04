0d1024e1b750491b495a103eaed02e117963ed6b
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-08-04 12:42:29 +0200

    explosions score and gameOver

--- js/gameRaven.js
+++ js/gameRaven.js
@@ -5,10 +5,19 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+
+const collisionCanvas = document.getElementById('collisionCanvas');
+const collisionCtx = collisionCanvas.getContext('2d');
+collisionCanvas.width = window.innerWidth;
+collisionCanvas.height = window.innerHeight;
+
+
 let timeToNextRaven = 0;
-let ravenInterval = 500;
+let ravenInterval = 1600;
 let lastTime = 0;
-let score = 0
+let score = 0;
+let gameOver = false;
+ctx.font = '50px Impact';
 
 let ravens = [];
 class Raven{
@@ -20,7 +29,7 @@ class Raven{
         this.height = this.spriteHeight * this.sizeModifier;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
-        this.directionX = Math.random() * 5 + 3;
+        this.directionX = Math.random() * 2 + 3;
         this.directionY = Math.random() * 5 - 2.5;
         this.markedForDeletion = false;
         this.image = new Image();
@@ -29,6 +38,9 @@ class Raven{
         this.maxFrame = 4;
         this.timeSinceFlap = 0;
         this.flapInterval = Math.random() * 70 + 70;
+        //color collision couleur sert de password unique
+        this.randomColors = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
+        this.color = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')';
     }
     update(deltaTime){
         if (this.y < 0 || this.y > canvas.height - this.height){
@@ -45,23 +57,84 @@ class Raven{
             this.timeSinceFlap = 0;
         }
         //console.log(deltaTime);
+        if(this.x < 0 - this.width) gameOver = true;
     }
     draw(){
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+        collisionCtx.fillStyle = this.color;
+        collisionCtx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
-function drawScore(){
-    ctx.fillStyle = 'white'
+let explosions = [];
+class Explosion {
+    constructor(x, y, size){
+        this.image = new Image();
+        this.image.src = 'images/boom.png';
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.frame = 0;
+        this.sound = new Audio();
+        this.sound.src = 'audio/boom.wav';
+        this.timeSinceLastFrame = 0;
+        this.frameInterval = 200;
+        this.markedForDeletion = false;
+
+    }
+    update(deltaTime){
+        if(this.frame === 0) this.sound.play();
+        this.timeSinceLastFrame += deltaTime;
+        if(this.timeSinceLastFrame > this.frameInterval){
+            this.frame ++;
+            this.timeSinceLastFrame = 0;
+            if(this.frame > 5) this.markedForDeletion = true;
+        }
+    }
+    draw(){
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y - this.size/4, this.size, this.size )
+    }
 }
 
 
+function drawScore(){
+    ctx.fillStyle = 'black';
+    ctx.fillText('Score: ' + score, 50, 75);
+    ctx.fillStyle = 'white';
+    ctx.fillText('Score: ' + score, 55, 80);
+}
+
+function drawGameOver(){
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'black';
+    ctx.fillText('GAME OVER, your score is ' + score, canvas.width/2, canvas.height/2);
+    ctx.fillStyle = 'white';
+    ctx.fillText('GAME OVER, your score is ' + score, canvas.width/2 + 5, canvas.height/2 +5);
+}
+
+window.addEventListener('click', function(e) {
+    //console.log(e.x, e.y); scan area 1 sur 1 pixel
+    const detectPixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1);
+    console.log(detectPixelColor);
+    const pc = detectPixelColor.data;
+    ravens.forEach(object => {
+        if(object.randomColors[0] === pc[0] && object.randomColors[1] === pc[1] && object.randomColors[2] === pc[2] ){
+            //collision detected
+            object.markedForDeletion = true;
+            score++;
+            explosions.push(new Explosion(object.x, object.y, object.width));
+            //console.log(explosions);
+        }
+    });
+});
 
 //const raven = new Raven(); verification avec 1 seul element
 
 function animate(timestamp){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    collisionCtx.clearRect(0, 0, canvas.width, canvas.height);
 
     let deltaTime =timestamp - lastTime;
     lastTime = timestamp;
@@ -71,21 +144,28 @@ function animate(timestamp){
     if (timeToNextRaven > ravenInterval){
         ravens.push(new Raven());
         timeToNextRaven = 0;
+        ravens.sort(function(a,b){
+            return a.width - b.width;
+        });
         //console.log(ravens);
-    }
+    };
     //console.log(timestamp); probleme la premiere valeur est undefined
     // raven.update();
     // raven.draw();
 
-    //array literal []  ... spread operator  particul class expense
-    [...ravens].forEach(object => object.update(deltaTime));
-    [...ravens].forEach(object => object.draw());
+    drawScore();
+
+    //array literal []  ... spread operator  particul class expense pour ajouter explosions array
+    [...ravens, ...explosions].forEach(object => object.update(deltaTime));
+    [...ravens, ...explosions].forEach(object => object.draw());
 
     //on crée un nouveau tableau filtré sur markedForDeletion = false
     ravens = ravens.filter(object => !object.markedForDeletion);
+    explosions = explosions.filter(object => !object.markedForDeletion);
     //console.log(ravens);
     
-    requestAnimationFrame(animate)
+    if(!gameOver) requestAnimationFrame(animate);
+    else drawGameOver();
 }
 
 //pour regler lepb timestamp undefined, on defini une valeur de départ de 0
