9232980560e61aae69f65b22ab789279ef063b65
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-08-12 01:27:27 +0200

    fly enemy

--- game_final/js/main.js
+++ game_final/js/main.js
@@ -1,6 +1,7 @@
 import Player from './player.js';
 import {InputHandler} from './input.js';
 import {Background} from './bakground.js';
+import {FlyingEnemy, GroundEnemy, ClimbingEnemy} from './enemies.js';
 
 window.addEventListener('load', function(){
 
@@ -20,15 +21,38 @@ window.addEventListener('load', function(){
             this.background = new Background(this);
             this.player = new Player(this);
             this.input = new InputHandler();
+            this.enemies = [];
+            this.enemyTimer = 0;
+            this.enemyInterval = 1000;
         }
         update(deltaTime) {
             this.background.update();
             this.player.update(this.input.keys, deltaTime);
+
+            //handleEnemy
+            if (this.enemyTimer > this.enemyInterval){
+                this.addEnemy();
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime;
+            }
+            this.enemies.forEach(enemy => {
+                enemy.update(deltaTime);
+                if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy),1);
+            })
+
         }
         draw(context) {
             this.background.draw(context);
             this.player.draw(context);
-        } 
+            this.enemies.forEach(enemy => {
+                enemy.draw(context );
+            })
+        }
+        addEnemy(){
+            this.enemies.push(new FlyingEnemy(this));
+            console.log(this.enemies);
+        }
     }
 
     const game = new Game(canvas.width, canvas.height)
